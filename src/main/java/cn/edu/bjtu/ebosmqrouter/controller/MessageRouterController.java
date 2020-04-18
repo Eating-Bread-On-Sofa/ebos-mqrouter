7421f7ab61fb4430629478d4490e88704c3271b1
Voo-C <voodoo.7@outlook.com>
2020-04-18 19:54:42 +0800

    add swagger

--- src/main/java/cn/edu/bjtu/ebosmqrouter/controller/MessageRouterController.java
+++ src/main/java/cn/edu/bjtu/ebosmqrouter/controller/MessageRouterController.java
@@ -6,6 +6,9 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import cn.edu.bjtu.ebosmqrouter.service.MqProducer;
 import cn.edu.bjtu.ebosmqrouter.util.dataAnalysis.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Date;
@@ -13,6 +16,7 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+@Api(tags = "消息路由")
 @RequestMapping("/api/router")
 @RestController
 public class MessageRouterController {
@@ -30,6 +34,8 @@ public class MessageRouterController {
         return mqProducer.getClass().toString();
     }
 
+    @ApiOperation(value = "添加消息路由",notes = "需name，incomingQueue，OutcomingQueue三个字段的JSON对象，分别为路由名称、接收消息队列名称、转发消息队列名称")
+    @ApiImplicitParam(name = "info",value = "路由信息",required = true,dataType = "JSONObject")
     @CrossOrigin
     @PostMapping()
     public String newRouter(@RequestBody JSONObject info){
@@ -39,23 +45,26 @@ public class MessageRouterController {
             Raw raw = new Raw(info.getString("name"),info.getString("incomingQueue"),info.getString("outgoingQueue"));
             status.add(info);
             threadPoolExecutor.execute(raw);
-            return "启动成功~~";}catch (Exception e){return "参数错误!";}
+            logService.info("添加新路由"+info.toString());
+            return "启动成功";}catch (Exception e){return "参数错误!";}
         }else {
             return "名称重复！";
         }
     }
 
-
+    @ApiOperation(value = "查看消息路由",notes = "返回JSON Array，每个Object为一个路由")
     @CrossOrigin
     @GetMapping()
     public JSONArray allInfo(){
         return status;
     }
 
-
+    @ApiOperation(value = "删除消息路由",notes = "需和GET方法中的信息相同，包括name，incomingQueue，OutcomingQueue，createTime")
+    @ApiImplicitParam(name = "info",value = "路由信息",required = true,dataType = "JSONObject")
     @CrossOrigin
     @DeleteMapping()
     public boolean delete(@RequestBody JSONObject info){
+        logService.info("删除路由"+info.toString());
         return status.remove(info);
     }
 
@@ -84,14 +93,8 @@ public class MessageRouterController {
     }
 
     @CrossOrigin
-    @RequestMapping ("/logtest")
-    public String logtest1(){
-        logService.info("mqrouter");
-        return "成功";
-    }
-    @CrossOrigin
-    @GetMapping("/logtest")
-    public String logtest2(){
-        return logService.findLogByCategory("info");
+    @GetMapping("/log")
+    public String getLog(){
+        return logService.findAll();
     }
 }
